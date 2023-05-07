77543178faedfc8514de230d33fc897de2e4580a
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-05-07 22:24:12 +0300

    initial screen added

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
+import { randomMinMax } from "./js/random-number.js";
+import { playAudio } from "./js/play-audio.js";
+
 const flyingObject = document.getElementById('square');
 const buttonOk = document.getElementById('ok');
 const infoText = document.getElementById('info-text');
-const viewWidth = document.body.clientWidth;
-const viewHeight = document.body.clientHeight;
 let rezultHuman = 0;
 let rezultComputer = 0;
 let set = 0;
@@ -11,25 +12,17 @@ let flyinInterval;
 let timerInterval;
 let winner = '';
 let rezults = '';
-let timerTime = 300; //0.01/
+let timerTime = 200;
+let roundTime = 200;
+let roundsCount = 5;
+let rooster_Size = 50;
 const changeInterval = 1000; //
-let timer = 0;
+// let timer = 0;
 const timerObject = document.getElementById('timer');
-const  soundObject = document.getElementById("myAudio"); 
 let overall = [];
 let finalist = "";
+let isItend = false;
 
-
-function playAudio() { 
-    soundObject.play(); 
-    soundObject.volume = 0.15;
-} 
-
-function randomMinMax(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
 
 
 function timerRunOut(){
@@ -39,8 +32,10 @@ function timerRunOut(){
 }
 
 function puttFlyingObjectInRandomPlace(){
-    const positionX = randomMinMax(0, (viewWidth-200));
-    const positionY = randomMinMax(0, (viewHeight-200));
+    const viewWidth = document.body.clientWidth;
+    const viewHeight = document.body.clientHeight;
+    const positionX = randomMinMax(0, (viewWidth-rooster_Size));
+    const positionY = randomMinMax(0, (viewHeight-rooster_Size));
     square.style.setProperty('--top', positionY + 'px');
     square.style.setProperty('--left', positionX  + 'px');
     square.style.display='block';
@@ -60,16 +55,13 @@ function roundEnd() {
         overall[round-1]=-1;
         winner = "Kompiuteris -> " + overall.filter(x => x===-1).length + " Laimejimu";
     };
-
-    console.log(overall);
-
     rezults += `Roundas: ${round} > Zmg: ${rezultHuman} - PC: ${rezultComputer}. NUGALETOJAS: ${winner} \n\r`;
-    document.getElementById('info').style.display = 'flex';
+    document.getElementById('info-wrapper').style.display = 'flex';
     infoText.innerText = rezults;
     rezultComputer = 0;
     rezultHuman = 0;
 
-    if (round >= 10){
+    if (round >= roundsCount){
         if (overall.reduce((a,b)=>a+b)>0){
             finalist = 'ZMOGUS';
         } else if(overall.reduce((a,b)=>a+b)===0){
@@ -81,6 +73,7 @@ function roundEnd() {
         round = 1;
         rezults = '';
         overall = [];
+        isItend = true;
         return;
     }
     round++;
@@ -95,27 +88,61 @@ if (timerTime <= 0){
     clearInterval(timerInterval);
     clearTimeout(flyinInterval);
     square.style.display='none';
-    timerTime = 300;
+    timerTime = roundTime;
     roundEnd();
 }
-}
+}  
 
+function buttonOkClicked() {
+    if (isItend){
+        isItend = false;
+        document.getElementById("start-wrapper").style.display = 'flex';
+        document.getElementById('info-wrapper').style.display = 'none';
+        timerObject.style.display = 'none';
 
-buttonOk.addEventListener("click", ()=>{
-    document.getElementById('info').style.display = 'none';
+    } else {
+    timerObject.style.display = 'block';
+    document.getElementById("start-wrapper").style.display = 'none';
+    document.getElementById('info-wrapper').style.display = 'none';
     timerInterval = setInterval(timeDownCounter, 100);
     puttFlyingObjectInRandomPlace();
     flyinInterval =  setTimeout(timerRunOut, changeInterval);
-});
+    timerTime = roundTime;
+    }
+};
 
+buttonOk.addEventListener("click", buttonOkClicked);
 
-flyingObject.addEventListener("click", ()=>{
+function theTargetWasClicked() {
     playAudio();    
     rezultHuman=rezultHuman+1;
     rezultComputer=rezultComputer;
     clearTimeout(flyinInterval);
     puttFlyingObjectInRandomPlace();
-    flyinInterval =  setTimeout(timerRunOut, 1000);  //
-});  
+    flyinInterval =  setTimeout(timerRunOut, 1000);
 
+};
 
+flyingObject.addEventListener("click", theTargetWasClicked);
+
+function roosterSize(){
+    rooster_Size = document.getElementById("rooster-size").value * 1.2 +30;
+    document.documentElement.style.setProperty('--rooster-size', rooster_Size + "px");
+}
+
+document.getElementById('rooster-size').addEventListener('input', roosterSize);
+
+function roundInterval() {
+    roundTime = document.getElementById("round-time").value*10;
+}
+
+document.getElementById('round-time').addEventListener('input', roundInterval);
+
+function howManyRound(){
+    roundsCount = document.getElementById("rounds-count").value;
+}
+
+document.getElementById('rounds-count').addEventListener('input', howManyRound);
+
+const btnStart=document.getElementById('btn-start');
+btnStart.addEventListener("click", buttonOkClicked);
